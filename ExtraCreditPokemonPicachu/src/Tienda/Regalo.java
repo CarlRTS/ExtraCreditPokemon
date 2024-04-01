4ad4550258b027e1fc488f9f94cd604c064133ea
sergioob4 <144851074+sergioob4@users.noreply.github.com>
2024-04-01 16:03:29 -0400

    Beta de los metodos de la tienda.

---
+++ ExtraCreditPokemonPicachu/src/Tienda/Regalo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Regalo {
+    String nombre;
+    int costo;
+    int relacion;
+    Object[] lista;
+    
+    public void disminucionWatts(int x){
+        int restante = 0;
+        restante = x-costo;
+        
+        aumentarRelacion();
+        
+        //agregarLista();
+        
+    }
+    
+    public void pregPokemon(){
+        
+    }
+    
+    public void aumentarRelacion(){
+        relacion++;
+    }
+    
+    /*public void agregar a la lista(){
+        lista.add(pokemon);
+    }
+    /*
+}
